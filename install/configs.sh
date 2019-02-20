c0a7d004f1fddc553ab4073233067e2cc645df21
Thewessen <sthewessen@gmail.com>
2019-02-20 09:38:53 +0100

    Trying to store gitpassw savely

--- install/configs.sh
+++ install/configs.sh
@@ -4,3 +4,5 @@
 git config --global user.name "Thewessen"
 git config --global user.email "sthewessen@gmail.com"
 git config --global core.editor "nvim"
+
+# To store password, see https://stackoverflow.com/questions/13385690/how-to-use-git-with-gnome-keyring-integration
