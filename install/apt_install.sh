94dfd2786b99a60d50e1abb80d95fecd0f1fe620
Thewessen <sthewessen@gmail.com>
2019-03-19 09:02:19 +0100

    Added copy to clipboard after mouse select in tmux... Need a program called xclip for it. Also added the program too apt_install

--- install/apt_install.sh
+++ install/apt_install.sh
@@ -7,7 +7,7 @@ if [[ -z $(command -v zsh) ]]; then
   echo "ZSH should be installed first!! Aborting!"
 elif [[ `uname` -eq Linux ]]; then
     #1e array of programs
-    programs=("cmake" "terminator" "tmux" "vim" "npm" "git" "nodejs" "js" "rhino" "haskell-platform" "python3" "python3-dev" "python3.6-dev" "python-dev" "ctags" "ttf-mscorefonts-installer" "fonts-inconsolata")
+    programs=("xclip" "cmake" "terminator" "tmux" "vim" "npm" "git" "nodejs" "js" "rhino" "haskell-platform" "python3" "python3-dev" "python3.6-dev" "python-dev" "ctags" "ttf-mscorefonts-installer" "fonts-inconsolata")
     for com in ${programs[@]}; do
         echo "Installing $com..."
         command -v $com || sudo apt install $com
