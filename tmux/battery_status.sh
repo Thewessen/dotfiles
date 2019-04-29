ab061c0b00e16fd4354732b4581e5a1a6d729827
Thewessen <sthewessen@gmail.com>
2019-04-29 22:21:44 +0200

    Changed more colors and icons for tmux laptop

--- tmux/battery_status.sh
+++ tmux/battery_status.sh
@@ -4,7 +4,7 @@ batt_stat_perc=`acpi -b | grep -oE '[0-9]+'%`
 batt_stat_num=`echo $batt_stat_perc | grep -oE '[0-9]+'`
 batt_discharging=`acpi -b | grep Dis`
 batt_discharging_zerorate=`acpi -b | grep "zero rate"`
-batt_icon=🔋 
+batt_icon=🕯 
 low_batt_icon=🕱 
 cabel_icon=🔌 
 
@@ -16,7 +16,7 @@ if [ "$batt_discharging" ]; then
       if [ $batt_stat_num -lt 20 ]; then
           batt_stat="#[fg=colour1]$batt_stat$low_batt_icon#[fg=colour7]"
       else
-          batt_stat="$batt_stat$batt_icon"
+          batt_stat="$batt_stat $batt_icon"
       fi
    else
        batt_stat="$batt_stat$cabel_icon"
